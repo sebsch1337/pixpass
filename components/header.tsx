47121d72fff561350c7e2f46068b4e0b00e34c05
Sebastian Scherbes <github@scherbes.de>
2024-07-18 22:50:20 +0200

    create Header component

--- components/header.tsx
+++ components/header.tsx
@@ -1,10 +1,11 @@
 import { Logo } from "./logo";
+import { Navigation } from "./navigation";
 
 export const Header = () => {
 	return (
-		<header className="w-full p-8 flex justify-between">
+		<header className="w-full py-8 px-8 md:px-16 flex justify-between">
 			<Logo />
-			<p>Menu</p>
+			<Navigation />
 		</header>
 	);
 };
