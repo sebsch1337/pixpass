5a8c9c6c4116f592c45c76866d959f600cede245
Sebastian Scherbes <github@scherbes.de>
2024-07-21 18:45:35 +0200

    move main to layout

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -3,13 +3,13 @@ import { HeroSection } from "./_components/hero-section";
 
 export default function Home() {
 	return (
-		<main className="flex-grow container mx-auto">
+		<>
 			<div className="max-w-5xl px-8 mx-auto">
 				<HeroSection />
 			</div>
 			<div className="max-w-5xl px-8 mx-auto mt-4 md:mt-14">
 				<FeaturesSection />
 			</div>
-		</main>
+		</>
 	);
 }
