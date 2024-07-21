97824fe8e47f6a6c68d09a6774d2e87c27b59663
Sebastian Scherbes <github@scherbes.de>
2024-07-21 18:45:05 +0200

    create 404 component

---
+++ app/not-found.tsx
@@ -0,0 +1,9 @@
+export default function NotFound() {
+	return (
+		<div className="h-[calc(100dvh-12rem)] w-full flex flex-col justify-center items-center">
+			<h2 className="text-xl">
+				<span className="font-bold">404</span> | Not Found
+			</h2>
+		</div>
+	);
+}
