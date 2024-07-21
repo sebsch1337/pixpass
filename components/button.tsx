bd9d143753051a311a1d01d7cc514e736a6407e0
Sebastian Scherbes <github@scherbes.de>
2024-07-21 18:46:34 +0200

    add outline styles to button component

--- components/button.tsx
+++ components/button.tsx
@@ -2,17 +2,19 @@ import { cn } from "@/utils/cn";
 
 const variants = {
 	flat: "bg-sky-500 hover:bg-sky-600 active:bg-sky-700 text-white disabled:bg-sky-100 disabled:text-sky-400",
-	outline: "bg-white text-sky-600 border-sky-600",
+	outline:
+		"bg-transparent border-2 text-sky-600 hover:text-sky-700 active:text-sky-800 disabled:text-sky-200 border-sky-600 hover:border-sky-700 active:border-sky-800 disabled:border-sky-200",
 };
 
 interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
-	variant: "flat" | "outline";
+	variant?: "flat" | "outline";
+	hero?: boolean;
 }
 
-export const Button = ({ variant, children, ...props }: ButtonProps) => {
+export const Button = ({ variant = "flat", hero = false, children, ...props }: ButtonProps) => {
 	return (
 		<button
-			className={cn(variants[variant], "w-full rounded-full font-semibold py-4 px-10 uppercase")}
+			className={cn(variants[variant], hero ? "py-4" : "py-2", "w-full rounded-full font-semibold px-10 uppercase")}
 			{...props}
 		>
 			{children}
