049f6ea3f74fdf1133c19d9c39683333fa38e946
Sebastian Scherbes <github@scherbes.de>
2024-07-27 12:15:47 +0200

    create ToolNavigation component

---
+++ app/tool/_components/tool-navigation.tsx
@@ -0,0 +1,44 @@
+import { Button } from "@/components/ui/button";
+
+import { usePicture } from "@/hooks/usePicture";
+import { useProgress } from "@/hooks/useProgress";
+
+export const ToolNavigation = () => {
+	const { picture } = usePicture();
+	const { progress, setProgress } = useProgress();
+
+	return (
+		<>
+			<div className="w-full md:w-auto">
+				{progress === "edit" && (
+					<Button
+						variant="outline"
+						onClick={() => setProgress("upload")}
+					>
+						&lt; Back
+					</Button>
+				)}
+				{progress === "download" && (
+					<Button
+						variant="outline"
+						onClick={() => setProgress("edit")}
+					>
+						&lt; Back
+					</Button>
+				)}
+			</div>
+			<div className="w-full md:w-auto">
+				{progress === "upload" && (
+					<Button
+						disabled={!!!picture}
+						onClick={() => setProgress("edit")}
+					>
+						Next &gt;
+					</Button>
+				)}
+				{progress === "edit" && <Button onClick={() => setProgress("download")}>Next &gt;</Button>}
+				{progress === "download" && <Button onClick={() => setProgress("upload")}>Finish</Button>}
+			</div>
+		</>
+	);
+};
