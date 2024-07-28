9fa6c8cb00d50dec78187789e02339e8d05a2eaa
Sebastian Scherbes <github@scherbes.de>
2024-07-28 18:35:36 +0200

    change header spacing

--- components/header.tsx
+++ components/header.tsx
@@ -3,7 +3,7 @@ import { Navigation } from "./navigation";
 
 export const Header = () => {
 	return (
-		<header className="w-full py-8 px-8 md:px-16 flex justify-between">
+		<header className="w-full py-8 px-8 flex justify-between">
 			<Logo />
 			<Navigation />
 		</header>
