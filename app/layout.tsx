07d4472f5e3d3af0c65015ddf4dd121fd17ad8db
Sebastian Scherbes <github@scherbes.de>
2024-07-28 18:42:51 +0200

    fix container placement

--- app/layout.tsx
+++ app/layout.tsx
@@ -23,11 +23,13 @@ export default function RootLayout({
 }>) {
 	return (
 		<html lang="en">
-			<body className={cn("bg-sky-50 text-sky-950 min-h-dvh flex flex-col md:container", inter.className)}>
-				<Header />
-				<main className="flex-grow container mx-auto">{children}</main>
-				<Footer />
-				<Toaster richColors />
+			<body className={cn("bg-sky-50 text-sky-950", inter.className)}>
+				<div className=" min-h-dvh flex flex-col justify-center items-center md:container">
+					<Header />
+					<main className="flex-grow container mx-auto">{children}</main>
+					<Footer />
+					<Toaster richColors />
+				</div>
 			</body>
 		</html>
 	);
