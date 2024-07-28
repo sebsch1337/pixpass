0bfb9f4d255e0204fcf518d61955d2129f935a5c
Sebastian Scherbes <github@scherbes.de>
2024-07-28 18:35:25 +0200

    add links to privacy and terms

--- components/footer.tsx
+++ components/footer.tsx
@@ -1,3 +1,14 @@
+import Link from "next/link";
+
 export const Footer = () => {
-	return <footer className="p-8 flex justify-center">Made with 🍣</footer>;
+	return (
+		<footer className="p-8 flex flex-col items-center justify-center gap-2">
+			<p>Made with 🍣</p>
+			<div className="flex flex-row gap-2 text-xs">
+				<Link href="/privacy">Privacy Policy</Link>
+				{`-`}
+				<Link href="/terms">Terms of Service</Link>
+			</div>
+		</footer>
+	);
 };
