cb799c9b58900325dab8b9a9f8af84cfe4576877
Sebastian Scherbes <github@scherbes.de>
2024-07-28 18:35:17 +0200

    add terms of service

---
+++ app/(home)/(routes)/terms/page.tsx
@@ -0,0 +1,64 @@
+const TermsAndConditions = () => {
+	return (
+		<div className="container mx-auto px-4 py-6">
+			<h1 className="text-2xl font-bold text-center mb-4">Terms and Conditions for PixPass</h1>
+			<p>
+				Welcome to PixPass! These terms and conditions outline the rules and regulations for the use of PixPass, a notes app
+				created by Sebastian Scherbes.
+			</p>
+
+			<h2 className="text-xl font-semibold mt-5">1. Acceptance of Terms</h2>
+			<p>
+				By accessing and using PixPass, you agree to comply with and be bound by the following terms and conditions. If you
+				disagree with any part of these terms, please do not use our application.
+			</p>
+
+			<h2 className="text-xl font-semibold mt-5">2. User Responsibilities</h2>
+			<p>
+				You are allowed to use PixPass in any manner that does not violate international law. Any use of the app that violates
+				such laws will result in immediate termination of your access.
+			</p>
+
+			<h2 className="text-xl font-semibold mt-5">3. Intellectual Property and Liability Limitations</h2>
+			<p>
+				The content created within PixPass by users remains their intellectual property. However, by using the app, you grant
+				PixPass a non-exclusive, worldwide, royalty-free license to use, reproduce, adapt, publish, translate and distribute your
+				content in any existing or future media. Sebastian Scherbes assumes no responsibility for any direct, indirect,
+				incidental, special, consequential or exemplary damages, including but not limited to damages for loss of profits,
+				goodwill, use, data or other intangible losses, resulting from the use of the app.
+			</p>
+
+			<h2 className="text-xl font-semibold mt-5">4. Privacy Policy</h2>
+			<p>For information about how we collect, use, and share your information, please refer to our Privacy Policy.</p>
+
+			<h2 className="text-xl font-semibold mt-5">5. Governing Law</h2>
+			<p>
+				These Terms shall be governed and construed in accordance with the laws of the Philippines, without regard to its conflict
+				of law provisions.
+			</p>
+
+			<h2 className="text-xl font-semibold mt-5">6. Changes to Terms</h2>
+			<p>
+				We reserve the right to modify or replace these Terms at any time. Changes will be communicated via email. By continuing
+				to access or use our app after those revisions become effective, you agree to be bound by the revised terms.
+			</p>
+
+			<h2 className="text-xl font-semibold mt-5">7. Account Termination</h2>
+			<p>
+				Your account may be terminated or suspended if you use the app excessively in a manner not intended for normal use, or if
+				you violate any international laws.
+			</p>
+
+			<h2 className="text-xl font-semibold mt-5">8. Dispute Resolution</h2>
+			<p>
+				In the event of any dispute arising in relation to these Terms and Conditions, the parties agree to engage in good faith
+				to resolve any such dispute.
+			</p>
+
+			<h2 className="text-xl font-semibold mt-5">9. Contact Us</h2>
+			<p>If you have any questions about these Terms, please contact us at pixpass-terms@scherbes.de.</p>
+		</div>
+	);
+};
+
+export default TermsAndConditions;
