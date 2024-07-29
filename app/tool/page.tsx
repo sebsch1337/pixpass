95130f0abb504df89a663ac10c8c918f65252e38
Sebastian Scherbes <github@scherbes.de>
2024-07-29 11:00:35 +0200

    add download page

--- app/tool/page.tsx
+++ app/tool/page.tsx
@@ -4,13 +4,15 @@ import { useEffect } from "react";
 
 import { ToolProgress } from "./_components/tool-progress";
 import { ToolNavigation } from "./_components/tool-navigation";
-
-import { useProgress } from "@/hooks/useProgress";
 import { ToolUploadInfo } from "./_components/tool-upload-info";
-import { ToolUploadPicture } from "./_components/tool-upload-picture";
 import { ToolEditFormat } from "./_components/tool-edit-format";
 import { ToolEditPicture } from "./_components/tool-edit-picture";
-import { ToolPictureAiCheck } from "./_components/tool-picture-ai-check";
+import { ToolEditAiCheck } from "./_components/tool-edit-ai-check";
+import { ToolUploadPicture } from "./_components/tool-upload-picture";
+import { ToolDownloadPreview } from "./_components/tool-download-preview";
+import { ToolDownloadButtons } from "./_components/tool-download-buttons";
+
+import { useProgress } from "@/hooks/useProgress";
 
 const ToolPage: React.FC = () => {
 	const { progress } = useProgress();
@@ -35,20 +37,23 @@ const ToolPage: React.FC = () => {
 
 			<div className="max-w-5xl mx-auto mt-8 flex flex-col md:flex-row gap-8">
 				<div className="flex-1 flex flex-col gap-8">
-					<section className="flex-1 rounded-xl bg-sky-200 flex flex-col h-full p-8">
+					<section className="flex-1 rounded-xl bg-sky-200 flex flex-col h-full p-6">
 						{progress === "upload" && <ToolUploadInfo />}
 						{progress === "edit" && <ToolEditFormat />}
+						{progress === "download" && <ToolDownloadButtons />}
 					</section>
 
-					<section className="flex-1 rounded-xl bg-sky-200 flex flex-col h-full p-8">
-						{/* {progress === "upload" && <ToolPictureAiCheck />} */}
-						{progress === "edit" && <ToolPictureAiCheck />}
-					</section>
+					{progress === "edit" && (
+						<section className="flex-1 rounded-xl bg-sky-200 flex flex-col h-full p-6">
+							<ToolEditAiCheck />
+						</section>
+					)}
 				</div>
 
-				<section className="flex-1 rounded-xl bg-sky-200 flex flex-col h-full p-8">
+				<section className="flex-1 rounded-xl bg-sky-200 flex flex-col h-full p-6">
 					{progress === "upload" && <ToolUploadPicture />}
 					{progress === "edit" && <ToolEditPicture />}
+					{progress === "download" && <ToolDownloadPreview />}
 				</section>
 			</div>
 
