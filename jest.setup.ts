62a54c024724f88a860d4c05ee6d0021c2f77559
Sebastian Scherbes <github@scherbes.de>
2024-08-01 17:22:34 +0200

    add npm functions

--- jest.setup.ts
+++ jest.setup.ts
@@ -1,2 +1,5 @@
-// jest.setup.ts
 import "@testing-library/jest-dom";
+import { TextEncoder, TextDecoder } from "util";
+
+global.TextEncoder = TextEncoder;
+global.TextDecoder = TextDecoder as unknown as typeof global.TextDecoder;
