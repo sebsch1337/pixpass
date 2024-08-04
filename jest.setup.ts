63ae2aad0513f252b89d1a457458f3166f933cf3
Sebastian Scherbes <github@scherbes.de>
2024-08-04 21:46:43 +0200

    install dotenv

--- jest.setup.ts
+++ jest.setup.ts
@@ -1,6 +1,10 @@
-import "@testing-library/jest-dom";
-import { TextEncoder, TextDecoder } from "util";
 import "whatwg-fetch";
+import "@testing-library/jest-dom";
+
+import dotenv from "dotenv";
+import { TextEncoder, TextDecoder } from "util";
+
+dotenv.config();
 
 global.TextEncoder = TextEncoder;
 global.TextDecoder = TextDecoder as unknown as typeof global.TextDecoder;
